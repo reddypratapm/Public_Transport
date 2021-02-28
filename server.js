5765a89fd5232366de29711a091847b298df0854
reddypratapm <57904650+reddypratapm@users.noreply.github.com>
2021-02-28 22:09:19 +0000

    Updated

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ var app = express();
 var Route;
 var Direction;
 var username1 = [];
+var username2 = [];
 
 app.set("view engine", "ejs");
-
-//app.set(__dirname); 
-//app.use(express.static(__dirname));
-
 
 
 app.use(bodyParser.json({extended: true}));
@@ -34,15 +31,50 @@ app.get("/", (req, res) => {
         switch (Route)
         {
             case '1B':
-                console.log(username1);
-                    return res.render("index", { username: username1});
-                
+                    return res.render("index", { data: { username: username1}});
+                    break;
             case '2B':
-                return res.render("index", { username: username1});
+                return res.render("index", { data: { username: username1}});
                 break;
             default:
-                return res.render("index", { username: ['Select Direction']});
+                return res.render("index", { data: {username: ['Select Direction']}});
                 break;
+        }
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+//after selecting the Route, this will give the Stops
+app.get("/what", (req, res) => {
+  mysqlConnection.query(
+    "SELECT * FROM direction",
+    (err, results, fields) => {
+      if (!err) {
+        for (var i = 0; i < 2; i++)
+        {
+            username2[i] = results[i][Direction];
+        }
+
+        switch (Direction)
+        {
+            case "Bangalore to Mangalore":
+                    return res.render("index", { data: { direction1: username2}});
+                    break;
+            case 'Hyderabad to Karimnagar':
+                    return res.render("index", { data: { direction1: username2}});
+                    break;
+            case 'Delhi to Assam':
+                    return res.render("index", { data: { direction1: username2}});
+                    break;
+            case 'Mumbai to Pune':
+                    return res.render("index", { data: { direction1: username2}});
+                    break;
+            default:
+                    return res.render("index", { data: {direction1: ['Select Stop']}});
+                    break;
         }
       } else {
         console.log(err);
@@ -54,13 +86,24 @@ app.get("/", (req, res) => {
 // select the route
 app.get('/create',function(req,res){
     Route = req.query.selectpicker;
-    console.log(Route);
+    console.log('Routevalue',Route);
     return res.redirect("/");
+});
+
+app.get('/direction',function(req,res){
+  Direction =(req.query.selectpicker1);
+  console.log('Directionvalue',Direction);
+  return res.redirect('/what');
 });
 
 //server to port 3000
 app.get('/', function(req, res) {
     res.render('index.ejs');
+});
+
+//final result
+app.get('/result', function(req, res) {
+  res.send({'Selected Route':Route,'Selected Direction':Direction,'Selected Stop':req.query.selectpicker});
 });
 
 
